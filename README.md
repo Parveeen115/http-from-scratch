f4498a526ee139cf5789a5809fa7752c418e9e11
Parveen <parveensharma1029384756@email.com>
2026-06-15 22:28:48 +0530

    batman

---
+++ README.md
@@ -0,0 +1,12 @@
+# http-from-scratch
+
+Building HTTP/1.1 from raw TCP in Go. No net/http, no frameworks.
+
+## Progress
+- [x] TCP listener and connection handling
+- [x] Reading raw bytes from connection
+- [x] Sending valid HTTP response
+- [ ] HTTP request parsing
+- [ ] Routing by path
+- [ ] POST body parsing
+- [ ] Persistent connections (keep-alive)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8000")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error :", err)
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("error", err)
+			return
+		}
+
+		fmt.Printf("Recived: %s\n", buffer[:n])
+
+		response := "HTTP/1.1 200 OK\r\n" +
+			"Content-Length: 13\r\n" +
+			"Content-Type: text/plain\r\n" +
+			"\r\n" +
+			"Hello, world!"
+		conn.Write([]byte(response))
+	}
+}
